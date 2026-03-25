21ec342a643e41c740286698ffdc042f3b65f5e7
Rust-Frog <b235.dev@gmail.com>
2026-03-26 02:31:12 +0800

    fix: enforce live Arch mirrors and pacman-key init in CI

--- src/network/nm/dbus_types.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <qdbusextratypes.h>
-#include <qmap.h>
-#include <qstring.h>
-#include <qvariant.h>
-
-using ConnectionSettingsMap = QMap<QString, QVariantMap>;
-Q_DECLARE_METATYPE(ConnectionSettingsMap);
--- src/services/pipewire/module.md
+++
@@ -1,9 +0,0 @@
-name = "Quickshell.Services.Pipewire"
-description = "Pipewire API"
-headers = [
-	"qml.hpp",
-	"peak.hpp",
-	"link.hpp",
-	"node.hpp",
-]
------
